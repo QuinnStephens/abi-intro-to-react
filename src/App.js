514a5ca8fee8b714649310026e11647193ee8ace
Quinn Stephens <quinn@quinnstephens.com>
2017-10-30 21:53:27 -0500

    Added callback prop

--- src/App.js
+++ src/App.js
@@ -5,16 +5,21 @@ import './App.css';
 class App extends Component {
   render() {
     let introText = "I came from a variable!"
+    this.introText = introText
     let headerText = "App says: Header says Hello!"
 
     return (
       <div className="App">
-        <Header text={headerText} />
+        <Header text={headerText} alert={this.showAlert.bind(this)} />
         <p className="App-intro">
           {introText}
         </p>
       </div>
     );
+  }
+
+  showAlert() {
+    alert('App says: ' + this.introText)
   }
 }
 
@@ -26,6 +31,7 @@ class Header extends Component {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">{this.props.text}</h1>
+        <button onClick={this.props.alert}>Click me!</button>
       </header>
       )
   }
