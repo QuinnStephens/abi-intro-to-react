008f999bacfc20a212fee1236b7b1e77e69ea30f
Quinn Stephens <quinn@quinnstephens.com>
2017-10-31 19:15:14 -0500

    Created basic counter

--- src/App.js
+++ src/App.js
@@ -8,40 +8,43 @@ class App extends Component {
     super()
 
     this.state = {
-      introText: "I came from the state!",
-      buttonPressedCount: 0
+      count: 0
     }
   }
 
   render() {
-    let headerText = "App says: Header says Hello!"
-
     return (
       <div className="App">
-        <Header text={headerText} alert={this.showAlert.bind(this)} />
-        <p className="App-intro">
-          {this.state.introText}
-        </p>
+        <Header />
+        <Counter count={this.state.count} />
+        <button onClick={this.incrementCount.bind(this)}>Add to count</button>
       </div>
     );
   }
 
-  showAlert() {
+  incrementCount() {
     this.setState({
-      buttonPressedCount: this.state.buttonPressedCount + 1
-    }, () => { alert('Button pressed ' + this.state.buttonPressedCount + ' times ') })
+      count: this.state.count + 1
+    })
   }
 }
 
 export default App;
+
+class Counter extends Component {
+  render() {
+    return (
+      <h1 className="Counter-text">{this.props.count}</h1>
+      )
+  }
+}
 
 class Header extends Component {
   render() {
     return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">{this.props.text}</h1>
-        <button onClick={this.props.alert}>Click me!</button>
+        <h1 className="App-title">My counter app</h1>
       </header>
       )
   }
